2d15441ef2abf5ca26addbea8ab517a4bb17c437
dravixcreate-lgtm <dravixcreate@gmail.com>
2026-05-10 17:50:28 +0530

    Create Theekha MirchaApplication.java

---
+++ src/main/java/com/theekhamircha/src/main/java/com/theekhamircha/src/main/java/com/theekhamircha/Theekha MirchaApplication.java
@@ -0,0 +1,11 @@
+package com.theekhamircha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheekhaMirchaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TheekhaMirchaApplication.class, args);
+    }
+}
